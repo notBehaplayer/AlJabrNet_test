6d052fd4efa95762f2c29966db5cb347828946ec
NotLolaty <bmutalov90@gmail.com>
2024-12-28 16:35:03 +0500

    fifth commit

---
+++ src/main/java/com/example/aljabrnet_test/AlJabrNetTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.aljabrnet_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlJabrNetTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlJabrNetTestApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/aljabrnet_test/AlJabrNetTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aljabrnet_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlJabrNetTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
